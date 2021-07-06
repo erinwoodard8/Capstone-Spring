921290bc77691ae3b4d3013900b23bb373be4aaa
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-06 10:55:43 -0400

    commit project files

---
+++ src/test/java/com/tts/CapstoneApp/CapstoneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.CapstoneApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
