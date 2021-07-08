2ec557a877e221b6513c9ea9ca5b2f5601637325
erinwoodard8 <erinwoodard8@gmail.com>
2021-07-08 08:35:32 -0400

    user controller set up

--- src/main/java/com/tts/CapstoneApp/Controller/UserController.java
+++ src/main/java/com/tts/CapstoneApp/Controller/UserController.java
@@ -2,26 +2,28 @@ package com.tts.CapstoneApp.Controller;
 
 
 import com.tts.CapstoneApp.Model.User;
-import com.tts.CapstoneApp.Service.UserServiceImpl;
+import com.tts.CapstoneApp.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
-    private UserServiceImpl userService;
+    private UserRepository userRepository;
 
-    @GetMapping("/get/{email}")
-    public User getUserByEmail(@PathVariable String email) {
-        return userService.findByEmail(email);
+
+    @PostMapping("/post")
+    public void addNewUser() {
+        User newUser = new User("testUser", "test@gmail.com", "password");
+        userRepository.save(newUser);
+        System.out.println(newUser);
     }
 
-    
+
 
 
 }
--- src/main/java/com/tts/CapstoneApp/Model/User.java
+++ src/main/java/com/tts/CapstoneApp/Model/User.java
@@ -25,12 +25,10 @@ import java.util.Arrays;
         public User () {
         }
 
-        public User (Integer id, String username, String email, String password, Long favoriteMovies[]) {
-            this.id = id;
+        public User (String username, String email, String password) {
             this.username = username;
             this.email = email;
             this.password = password;
-            this.favoriteMovies = favoriteMovies;
         }
     //GETTERS
 
